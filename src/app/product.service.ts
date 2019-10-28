5c34bef9801ecacd367906eec1753cd806518dd7
Israel Oluwole <olaleyeisrael@gmail.com>
2019-10-28 22:50:00 +0100

    inserted http call imp...

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { map } from 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
+  private _albumUrl: string = '../assets/album.json';
 
-  constructor() { }
+  constructor(
+    private _http: Http
+  ) { }
 
+  getAlbum(id: number) {
+    return this._http
+      .get(this._albumUrl)
+      .map((response: Response) => response.json());
+  }
 }
