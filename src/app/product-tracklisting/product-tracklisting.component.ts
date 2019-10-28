9de9bd51056a433950ee4458fa07adb07e754faa
Israel Oluwole <olaleyeisrael@gmail.com>
2019-10-28 23:41:45 +0100

    imports and data getting

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ProductService } from '../product.service';
+
+import { Album } from '../album';
+
 @Component({
   selector: 'app-product-tracklisiing',
   templateUrl: './product-tracklisiing.component.html',
   styleUrls: ['./product-tracklisiing.component.css']
 })
 export class ProductTracklistingComponent implements OnInit {
+  albumInfo: Album;
 
-  constructor() { }
+  constructor(
+    private _productService: ProductService
+  ) { }
 
   ngOnInit() {
+    this._productService
+      .getAlbum(1)
+      .subscribe(response => this.albumInfo = response);
   }
-
 }
