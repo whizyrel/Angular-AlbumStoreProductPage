59ad81b52173d91b65e5fadbdc2c29430c032cf2
Israel Oluwole <olaleyeisrael@gmail.com>
2019-10-29 00:04:37 +0100

    used service: product service and fixed class prop name

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,13 +10,16 @@ import { Product } from '../product';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-  product: Product[];
+  products: Product[];
 
   constructor(
     private _productService: ProductService
   ) { }
 
   ngOnInit() {
+    this._productService
+      .getProducts()
+      .subscribe(response => this.products = response);
   }
 
 }
